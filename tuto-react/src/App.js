ab500a9f97991b8a66d07ed6c52a7bade21455e7
moise <mose.ns@hotmail.com>
2020-08-15 21:31:16 +0200

    Todo class

--- tuto-react/src/App.js
+++ tuto-react/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos.js'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos:[
+        {
+          id: 1,
+          title: 'Learn Js',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Learn React',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Go for a walk',
+          completed: false
+        }
+      ]
+    }
+  }
+
+  
+
+  render(){
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+       <Todos/>
+  
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
