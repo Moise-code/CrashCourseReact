27e734516737119e22eb8cf75f2398698ead3f69
moise <mose.ns@hotmail.com>
2020-09-08 22:41:30 +0200

    Router part 1

--- crash-course/src/App.js
+++ crash-course/src/App.js
@@ -3,7 +3,12 @@ import HelloWorld from './components/helloworld'
 //import CounterExemple from './components/counterExemple'
 import Header from './components/Header'
 import Footer from './components/footer';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 
 function App() {
@@ -11,6 +16,15 @@ function App() {
     <div>
       <Header />
       <HelloWorld name='Chris'/>
+
+      <Router>
+      <ul>
+        <li>
+          <Link to='/' className='text-blue-500'>Home</Link>
+        </li>
+      </ul>
+      </Router>
+
       <Footer/>
     </div>
   );
