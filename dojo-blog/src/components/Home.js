71b62809a9e49c905b78967eed16fd177bea7db7
moise <mose.ns@hotmail.com>
2020-12-29 17:00:21 +0100

    click event

--- dojo-blog/src/components/Home.js
+++ dojo-blog/src/components/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log('Hello little fella!');
+  }
+
+  const handleClickAgain = (name) => {
+    console.log(`Hello ${name}`);
+  }
+
   return (
     <div className='home'>
       <h2>Home Page</h2>
+      <button
+      onClick={handleClick}
+      >Click me</button>
+      <button
+      onClick={() => handleClickAgain('Amanda')}
+      >Click me Again</button>
 
     </div>
   );
