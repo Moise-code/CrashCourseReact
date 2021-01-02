6dc6dbb0b80c1aee1f89d911ffdb2157c8594cae
moise <mose.ns@hotmail.com>
2021-01-02 02:48:55 +0100

    Applying useState hooks

--- dojo-blog/src/components/Home.js
+++ dojo-blog/src/components/Home.js
@@ -1,25 +1,17 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
+
 
 const Home = () => {
-  const [name, setName] = useState('Amanda Tapping');
-  const [age, setAge] = useState('52')
-
-  const handleClick = () => {
-    setName('Helene Magnus');
-    setAge('200');
-
-  }
-
-  
+  const [blogs, setBlogs] = useState([
+    {title: 'Fantastic beasts and how to catch them!', body: 'Lorem ipsum', author: 'Norbert Dragonneau', id: 1},
+    {title: 'Hunting like a pro', body: 'Carried away', author: 'Dean & Sam Winchester', id: 2},
+    {title: '10000 ways to mess with timeline', body: 'What did you learn? you bloody bastard!', author: 'Stewie G. Griffin', id: 3},
+  ]);
 
   return (
     <div className='home'>
-      <h2>Home Page</h2>
-      <p>{name} is {age} years old!</p>
-      <button
-      onClick={handleClick}
-      >Click me</button>
-      
+     <BlogList blogs={blogs} title='All Blogs'/>
 
     </div>
   );
