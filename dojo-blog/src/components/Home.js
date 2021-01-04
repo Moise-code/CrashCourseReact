1c946bd1103a9e25c06ab8a20b768aecb80665c5
moise <mose.ns@hotmail.com>
2021-01-04 18:20:46 +0100

    Reusing component

--- dojo-blog/src/components/Home.js
+++ dojo-blog/src/components/Home.js
@@ -7,11 +7,14 @@ const Home = () => {
     {title: 'Fantastic beasts and how to catch them!', body: 'Lorem ipsum', author: 'Norbert Dragonneau', id: 1},
     {title: 'Hunting like a pro', body: 'Carried away', author: 'Dean & Sam Winchester', id: 2},
     {title: '10000 ways to mess with timeline', body: 'What did you learn? you bloody bastard!', author: 'Stewie G. Griffin', id: 3},
+    {title: 'Build your time machine', body: 'What did you learn? you bloody bastard!', author: 'Stewie G. Griffin', id: 4},
+    {title: 'Go to a time you already exist', body: 'What did you learn? you bloody bastard!', author: 'Stewie G. Griffin', id: 5},
   ]);
 
   return (
     <div className='home'>
      <BlogList blogs={blogs} title='All Blogs'/>
+     <BlogList blogs={blogs.filter(blog => blog.author === 'Stewie G. Griffin')} title='Stewie s Blogs'/>
 
     </div>
   );
