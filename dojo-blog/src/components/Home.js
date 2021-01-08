faf623f2ee5e051f422e1995b00a507d3e7b694a
moise <mose.ns@hotmail.com>
2021-01-08 23:14:15 +0100

    Using Json server

--- dojo-blog/src/components/Home.js
+++ dojo-blog/src/components/Home.js
@@ -3,15 +3,8 @@ import BlogList from './BlogList';
 
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {title: 'Fantastic beasts and how to catch them!', body: 'Lorem ipsum', author: 'Norbert Dragonneau', id: 1},
-    {title: 'Hunting like a pro', body: 'Carried away', author: 'Dean & Sam Winchester', id: 2},
-    {title: '10000 ways to mess with timeline', body: 'What did you learn? you bloody bastard!', author: 'Stewie G. Griffin', id: 3},
-    {title: 'Build your time machine', body: 'What did you learn? you bloody bastard!', author: 'Stewie G. Griffin', id: 4},
-    {title: 'Go to a time you already exist', body: 'What did you learn? you bloody bastard!', author: 'Stewie G. Griffin', id: 5},
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
-  const [name, setName] = useState('stewie');
 
 
   const handleDelete = (id) => {
@@ -22,8 +15,7 @@ const Home = () => {
   //useEffect after the component render
   useEffect(() => {
     console.log('I am running after the component has rendered!');
-    console.log(name);
-  }, [name]);
+  }, []);
   
 
   return (
@@ -33,16 +25,32 @@ const Home = () => {
       title='All Blogs' 
       handleDelete={handleDelete} 
     />
-     {/* <BlogList 
-      blogs={blogs.filter(blog => blog.author === 'Stewie G. Griffin')} 
-      title='Stewie s Blogs'
-      handleDelete={handleDelete}
-    /> */}
-    <p>{ name }</p>
-    <button onClick={() => setName('Brian')}>Change Name</button>
+     
     
     </div>
   );
 }
 
 export default Home;
+
+
+/*
+ <BlogList 
+      blogs={blogs.filter(blog => blog.author === 'Stewie G. Griffin')} 
+      title='Stewie s Blogs'
+      handleDelete={handleDelete}
+    /> 
+    <p>{ name }</p>
+    <button onClick={() => setName('Brian')}>Change Name</button>
+
+    sudo npx json-server --watch data/db.json --port 8000
+
+
+    //END POINT USED 
+    
+    /blogs                  GET               Fetch all blogs
+    /blogs/{id}             GET               Fetch a single blog
+    /blogs                  POST              Add a new blog
+    /blogs/{id}             DELETE            Delete a blog
+
+*/
