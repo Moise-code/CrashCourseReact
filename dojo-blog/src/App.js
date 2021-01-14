2fe317d94c4fd75658d9d637076ab3153c61eedc
moise <mose.ns@hotmail.com>
2021-01-14 19:26:48 +0100

    React Router Exact path Route

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,16 +1,31 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './components/Create';
+import Error from './components/Error';
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-      <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/error">
+            <Error />
+          </Route>
+        </Switch>
+        </div>
+
       </div>
-      
-    </div>
+    </Router>
   );
 }
 
---
+++ dojo-blog/src/components/Error.js
@@ -0,0 +1,11 @@
+const Error = () => {
+  return(
+    <div className="errors">
+      <h2>Oops, that page does not exist!</h2>
+      <a href="/" className="back">Back to Home!</a>
+    </div>
+  );
+}
+
+export default Error;
+
