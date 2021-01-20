08bd51afd86432f1fa8b96e935c6ee952a9ed154
moise <mose.ns@hotmail.com>
2021-01-20 17:23:04 +0100

    Making a post request

--- dojo-blog/src/components/Create.js
+++ dojo-blog/src/components/Create.js
@@ -1,14 +1,31 @@
 import { useState } from 'react';
 
+
+
 const Create = () => {
   const[title, setTitle] = useState('');
   const[body, setBody] = useState('');
   const[author, setAuthor] = useState('mario');
+  const [isLoading, setIsLoading] = useState(false);
+
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+    setIsLoading(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    })
+    .then(() => {
+      console.log('new blog added.');
+      setIsLoading(false);
+  
+    })
+
   }
 
   return (
@@ -36,7 +53,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add blog</button>
+        {!isLoading && <button>Add blog</button>}
+        {isLoading && <button>Adding blog...</button>}
         
       </form>
     </div>
